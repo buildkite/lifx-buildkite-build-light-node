4739c15c3977667d08383e9a9c6db76c9407d1a2
Tim Lucas <t@toolmantim.com>
2015-04-07 17:48:36 +1000

    New events

--- index.js
+++ index.js
@@ -20,45 +20,46 @@ app.post('/', function(req, res){
     return res.status(401).send('Invalid token');
   }
 
-  // Process build event
-  if (req.headers['x-buildkite-event'] == 'build') {
+  var buildkiteEvent = req.headers['x-buildkite-event'];
 
-    switch (req.body.build.state) {
-      case 'running':
-        console.log('Build running');
-        post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
-          power_on:   false,
-          color:      "yellow brightness:5%",
-          from_color: "yellow brightness:35%",
-          period:     5,
-          cycles:     9999,
-          persist:    true
-        });
-        break;
-      case "passed":
-        console.log("Build passed");
-        post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
-          power_on:   false,
-          color:      "green brightness:75%",
-          from_color: "green brightness:10%",
-          period:     0.45,
-          cycles:     3,
-          persist:    true,
-          peak:       0.2
-        });
-        break;
-      case "failed":
-        console.log("Build failed");
-        post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
-          power_on:   false,
-          color:      "red brightness:60%",
-          from_color: "red brightness:25%",
-          period:     0.1,
-          cycles:     20,
-          persist:    true,
-          peak:       0.2
-        });
-        break;
+  // Process build.running event
+  if (buildkiteEvent == 'build.running') {
+    console.log('Build running');
+    post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
+      power_on:   false,
+      color:      "yellow brightness:5%",
+      from_color: "yellow brightness:35%",
+      period:     5,
+      cycles:     9999,
+      persist:    true
+    });
+
+  // Process build.finished event
+  } else if (buildkiteEvent == 'build.finished') {
+    var buildState = req.body.build.state;
+
+    if (buildState == "passed") {
+      console.log("Build passed");
+      post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
+        power_on:   false,
+        color:      "green brightness:75%",
+        from_color: "green brightness:10%",
+        period:     0.45,
+        cycles:     3,
+        persist:    true,
+        peak:       0.2
+      });
+    } else {
+      console.log("Build failed");
+      post_to_lifx("/v1beta1/lights/" + bulb_selector + "/effects/breathe.json", {
+        power_on:   false,
+        color:      "red brightness:60%",
+        from_color: "red brightness:25%",
+        period:     0.1,
+        cycles:     20,
+        persist:    true,
+        peak:       0.2
+      });
     }
   }
 
